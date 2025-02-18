ac84363d54c75f0872b76af760ea194e6e6a4d2d
ranyparihar <ranyparihar@gmail.com>
2025-02-19 01:36:10 +0530

    Project Changes

--- src/Component/ProjectGallery.js
+++ src/Component/ProjectGallery.js
@@ -5,13 +5,19 @@ import './ProjectGallery.css';
 import Navbar from './Navbar';
 import star from './star.svg';
 import Footer from './Footer.js';
-import PG1 from './PG1.png';
-import PG3 from './PG3.png';
-import PG2 from './PG2.png';
+import P1 from './P1.gif';
+import p3 from './Kuberx_f.gif';
+import ef from './ef.jpg';
+import p4 from './Ikansh.png';
+import p5 from "./PayCC.jpg"
+import cc from "./CC.png";
+import P2 from './ikansh.svg';
+import KuberX from './KX.jpg';
+import commingsoon from "./Coomingsoon.gif"
+
 import Contactpage from './ContactPage';
-import p11 from './ef.jpg';
-import p12 from './p11.svg';
-import p13 from './p11.svg';
+
+
 
 const Card = ({ title, date, duration, image, buttons, link, popupImage, onImageClick }) => (
   <div className="cardd" onClick={() => onImageClick(popupImage)}>
@@ -32,7 +38,7 @@ const Card = ({ title, date, duration, image, buttons, link, popupImage, onImage
       
       {duration && <p>{duration}</p>}
     </div>
-    <button className="play-button">▶</button>
+    {/* <button className="play-button">▶</button> */}
   </div>
 );
 
@@ -45,58 +51,18 @@ const ProjectGallery = () => {
 
   const projects = [
     {
-      title: "I enhanced ELECTRIFUEL's brand & digital presence.",
-      date: "Client : Electrifuel",
-      image: PG1,
-      popupImage: p11, // Image for the pop-up
+      title: "Innovating company Business report Solution ",
+      date: "Company : Credflow",
+      image: p3,
+      popupImage: KuberX, // Image for the pop-up
       buttons: [
+  
         {
-          label: "Logo Design",
+          label: "Product Design",
           style: {
             backgroundColor: 'rgb(2 110 58)',
             color: 'white',
-            padding: '12px 20px',
-            borderRadius: '20px',
-            border: 'none',
-            margin: '0 5px 5px 0'
-          }
-        },
-        {
-          label: "Brand Identity",
-          style: {
-            backgroundColor: 'rgb(2 110 58)',
-            color: 'white',
-            padding: '12px 20px',
-            borderRadius: '20px',
-            border: 'none',
-            margin: '0 5px 5px 0'
-          }
-        },
-        {
-          label: "Web Design",
-          style: {
-            backgroundColor: 'rgb(2 110 58)',
-            color: 'white',
-            padding: '12px 20px',
-            borderRadius: '20px',
-            border: 'none',
-            margin: '0 5px 5px 0'
-          }
-        }
-      ],
-    },
-    {
-      title: "Crafting Website & App UI for Service-Based Company 8Seva",
-      date: "Client: 8Seva",
-      image: PG2,
-      popupImage: p12, // Image for the pop-up
-      buttons: [
-        {
-          label: "Logo Design",
-          style: {
-            backgroundColor: '#9676ff',
-            color: 'white',
-            padding: '12px 20px',
+            padding: '8px 16px',
             borderRadius: '20px',
             border: 'none',
             margin: '0 5px 5px 0'
@@ -105,20 +71,9 @@ const ProjectGallery = () => {
         {
           label: "Web/App Design",
           style: {
-            backgroundColor: '#9676ff',
+            backgroundColor: 'rgb(2 110 58)',
             color: 'white',
-            padding: '12px 20px',
-            borderRadius: '20px',
-            border: 'none',
-            margin: '0 5px 5px 0'
-          }
-        },
-        {
-          label: "Social Media",
-          style: {
-            backgroundColor: '#9676ff',
-            color: 'white',
-            padding: '12px 20px',
+            padding: '8px 16px',
             borderRadius: '20px',
             border: 'none',
             margin: '0 5px 5px 0'
@@ -127,39 +82,61 @@ const ProjectGallery = () => {
       ],
     },
     {
-      title: "Creating a brand identity for the startup ikaansh",
-      date: "Client : IKAANSH",
-      image: PG3,
-      popupImage: p13, // Image for the pop-up
+      title: "Flexible business payments, Pay Via Credit Card feature",
+      date: "Company : Credflow",
+      image: cc,
+      popupImage: p5, // Image for the pop-up
       buttons: [
+
         {
-          label: "Branding",
+          label: "App Design",
           style: {
-            backgroundColor: 'rgb(188 53 3)',
+            backgroundColor: '#9676ff',
             color: 'white',
-            padding: '12px 20px',
+            padding: '8px 16px',
             borderRadius: '20px',
             border: 'none',
             margin: '0 5px 5px 0'
           }
         },
         {
-          label: "Logo Design",
+          label: "User Reaserch",
+          style: {
+            backgroundColor: '#9676ff',
+            color: 'white',
+            padding: '8px 16px',
+            borderRadius: '20px',
+            border: 'none',
+            margin: '0 5px 5px 0'
+          }
+        }
+      ],
+    },
+    {
+     
+      title: "I enhanced ELECTRIFUEL's brand & digital presence.",
+      date: "Client : Electrifuel",
+      image: P1,
+      popupImage: ef, // Image for the pop-up
+      buttons: [
+
+        {
+          label: "Brand Design",
           style: {
             backgroundColor: 'rgb(188 53 3)',
             color: 'white',
-            padding: '12px 20px',
+            padding: '8px 16px',
             borderRadius: '20px',
             border: 'none',
             margin: '0 5px 5px 0'
           }
         },
         {
-          label: "Marketing Collateral",
+          label: "Web Designing",
           style: {
             backgroundColor: 'rgb(188 53 3)',
             color: 'white',
-            padding: '5px 10px',
+            padding: '8px 16px',
             borderRadius: '20px',
             border: 'none',
             margin: '0 5px 5px 0'
@@ -168,6 +145,103 @@ const ProjectGallery = () => {
       ],
     },
   ];
+
+  const projects2 = [
+    {
+      title: "Creating a brand identity for the startup ikaansh",
+      date: "Client : IKAANSH",
+      image: p4,
+      popupImage: P2, // Image for the pop-up
+      buttons: [
+  
+        {
+          label: "Brand Identity",
+          style: {
+            backgroundColor: 'rgb(2 110 58)',
+            color: 'white',
+            padding: '8px 16px',
+            borderRadius: '20px',
+            border: 'none',
+            margin: '0 5px 5px 0'
+          }
+        },
+        {
+          label: "Marketing",
+          style: {
+            backgroundColor: 'rgb(2 110 58)',
+            color: 'white',
+            padding: '8px 16px',
+            borderRadius: '20px',
+            border: 'none',
+            margin: '0 5px 5px 0'
+          }
+        }
+      ],
+    },
+    {
+      title: "--",
+      date: "--",
+      image: commingsoon,
+      popupImage: "", // Image for the pop-up
+      buttons: [
+
+        {
+          label: "--",
+          style: {
+            backgroundColor: '#9676ff',
+            color: 'white',
+            padding: '8px 16px',
+            borderRadius: '20px',
+            border: 'none',
+            margin: '0 5px 5px 0'
+          }
+        },
+        {
+          label: "--",
+          style: {
+            backgroundColor: '#9676ff',
+            color: 'white',
+            padding: '8px 16px',
+            borderRadius: '20px',
+            border: 'none',
+            margin: '0 5px 5px 0'
+          }
+        }
+      ],
+    },
+    {
+      title: "--",
+      date: "--",
+      image: commingsoon,
+      popupImage:"", // Image for the pop-up
+      buttons: [
+
+        {
+          label: "--",
+          style: {
+            backgroundColor: 'rgb(188 53 3)',
+            color: 'white',
+            padding: '8px 16px',
+            borderRadius: '20px',
+            border: 'none',
+            margin: '0 5px 5px 0'
+          }
+        },
+        {
+          label: "--",
+          style: {
+            backgroundColor: 'rgb(188 53 3)',
+            color: 'white',
+            padding: '8px 16px',
+            borderRadius: '20px',
+            border: 'none',
+            margin: '0 5px 5px 0'
+          }
+        }
+      ],
+    },
+  ];
+  
 
   return (
     <div id="project-gallery">
@@ -183,11 +257,16 @@ const ProjectGallery = () => {
       <div className="contactpage-content">
         <div className="contactpage-left-section">
           <h1 style={{ fontSize: '44px' }}>Project <span className='subtitle-pg'>Gallery</span></h1>
-          <p style={{ fontSize: '18px', fontFamily:'Poppins'}}>A Comprehensive Showcase of My Most Impactful Projects</p>
+          <p style={{ fontSize: '16px', fontFamily:'Poppins' , fontWeight:"400", lineHeight:"32px"}}>A Comprehensive Showcase of My Most Impactful Projects</p>
         </div>
       </div>
       <div className="project-gallery-card">
         {projects.map((project, index) => (
+          <Card key={index} {...project} onImageClick={setSelectedImage} />
+        ))}
+      </div>
+      <div className="project-gallery-card">
+        {projects2.map((project, index) => (
           <Card key={index} {...project} onImageClick={setSelectedImage} />
         ))}
       </div>
